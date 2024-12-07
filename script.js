fb0e9ab6f69737ed866ede3d2d3e4f0f46f94d8e
Adrian <adrian.gazdowicz@gmail.com>
2024-12-07 18:02:23 +0100

    sasasd

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const images = ["images/obroza1.jpg", "images/obroza2.jpg", "images/obroza3.jpg"];
 let currentIndex = 0;
+let autoSlideInterval;
 
 function changeSlide(direction) {
     currentIndex += direction;
@@ -12,4 +13,36 @@ function changeSlide(direction) {
 
     const sliderImage = document.getElementById("slider-image");
     sliderImage.src = images[currentIndex];
+
+    resetAutoSlide();
 }
+
+function startAutoSlide() {
+    autoSlideInterval = setInterval(() => changeSlide(1), 5000);
+}
+
+function resetAutoSlide() {
+    clearInterval(autoSlideInterval);
+    startAutoSlide();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    startAutoSlide();
+});
+
+function revealSections() {
+    const sections = document.querySelectorAll('main > section');
+    const triggerBottom = window.innerHeight * 0.8;
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < triggerBottom) {
+            section.classList.add('visible');
+        }
+    });
+}
+
+window.addEventListener('scroll', revealSections);
+document.addEventListener('DOMContentLoaded', revealSections);
+
